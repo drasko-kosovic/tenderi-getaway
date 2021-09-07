d973a396d726add86e12984f2d0e460ce4cfff58
drasko-kosovic <drasko.kosovic@gmail.com>
2021-09-07 22:41:41 +0200

    init

---
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+    RouterModule.forChild([
+      {
+        path: 'ponude',
+        data: { pageTitle: 'tenderiApp.otvoreniPonude.home.title' },
+        loadChildren: () => import('./otvoreni/ponude/ponude.module').then(m => m.OtvoreniPonudeModule),
+      },
+      {
+        path: 'postupci',
+        data: { pageTitle: 'tenderiApp.otvoreniPostupci.home.title' },
+        loadChildren: () => import('./otvoreni/postupci/postupci.module').then(m => m.OtvoreniPostupciModule),
+      },
+      {
+        path: 'ponudjaci',
+        data: { pageTitle: 'tenderiApp.otvoreniPonudjaci.home.title' },
+        loadChildren: () => import('./otvoreni/ponudjaci/ponudjaci.module').then(m => m.OtvoreniPonudjaciModule),
+      },
+      {
+        path: 'narucilac',
+        data: { pageTitle: 'tenderiApp.otvoreniNarucilac.home.title' },
+        loadChildren: () => import('./otvoreni/narucilac/narucilac.module').then(m => m.OtvoreniNarucilacModule),
+      },
+      {
+        path: 'ugovor',
+        data: { pageTitle: 'tenderiApp.otvoreniUgovor.home.title' },
+        loadChildren: () => import('./otvoreni/ugovor/ugovor.module').then(m => m.OtvoreniUgovorModule),
+      },
+      {
+        path: 'specifikacije',
+        data: { pageTitle: 'tenderiApp.otvoreniSpecifikacije.home.title' },
+        loadChildren: () => import('./otvoreni/specifikacije/specifikacije.module').then(m => m.OtvoreniSpecifikacijeModule),
+      },
+      {
+        path: 'anex',
+        data: { pageTitle: 'tenderiApp.otvoreniAnex.home.title' },
+        loadChildren: () => import('./otvoreni/anex/anex.module').then(m => m.OtvoreniAnexModule),
+      },
+      /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
+    ]),
+  ],
+})
+export class EntityRoutingModule {}
---
+++ src/main/webapp/app/layouts/navbar/navbar.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { SessionStorageService } from 'ngx-webstorage';
+
+import { VERSION } from 'app/app.constants';
+import { LANGUAGES } from 'app/config/language.constants';
+import { AccountService } from 'app/core/auth/account.service';
+import { LoginService } from 'app/login/login.service';
+import { ProfileService } from 'app/layouts/profiles/profile.service';
+import { LoginComponent } from 'app/login/login.component';
+import { MatDialog } from '@angular/material/dialog';
+
+@Component({
+  selector: 'jhi-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+})
+export class NavbarComponent implements OnInit {
+  inProduction?: boolean;
+  isNavbarCollapsed = true;
+  languages = LANGUAGES;
+  openAPIEnabled?: boolean;
+  version = '';
+
+  constructor(
+    private loginService: LoginService,
+    private translateService: TranslateService,
+    private sessionStorage: SessionStorageService,
+    private accountService: AccountService,
+    private profileService: ProfileService,
+    private router: Router,
+    protected dialog: MatDialog
+  ) {
+    if (VERSION) {
+      this.version = VERSION.toLowerCase().startsWith('v') ? VERSION : 'v' + VERSION;
+    }
+  }
+
+  ngOnInit(): void {
+    this.profileService.getProfileInfo().subscribe(profileInfo => {
+      this.inProduction = profileInfo.inProduction;
+      this.openAPIEnabled = profileInfo.openAPIEnabled;
+    });
+  }
+
+  changeLanguage(languageKey: string): void {
+    this.sessionStorage.store('locale', languageKey);
+    this.translateService.use(languageKey);
+  }
+
+  collapseNavbar(): void {
+    this.isNavbarCollapsed = true;
+  }
+
+  isAuthenticated(): boolean {
+    return this.accountService.isAuthenticated();
+  }
+
+  login(): void {
+    this.router.navigate(['/login']);
+  }
+  addPrijava(): any {
+    this.dialog.open(LoginComponent);
+  }
+  logout(): void {
+    this.collapseNavbar();
+    this.loginService.logout();
+    this.router.navigate(['']);
+  }
+
+  toggleNavbar(): void {
+    this.isNavbarCollapsed = !this.isNavbarCollapsed;
+  }
+
+  getImageUrl(): string {
+    return this.isAuthenticated() ? this.accountService.getImageUrl() : '';
+  }
+}
