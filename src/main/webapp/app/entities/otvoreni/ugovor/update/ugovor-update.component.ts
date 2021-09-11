475ace60309e2999db9d398e00975574b4b7be1e
drasko-kosovic <drasko.kosovic@gmail.com>
2021-09-11 20:26:07 +0200

    ugovor

--- src/main/webapp/app/entities/otvoreni/ugovor/update/ugovor-update.component.ts
+++ src/main/webapp/app/entities/otvoreni/ugovor/update/ugovor-update.component.ts
@@ -1,85 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { HttpResponse } from '@angular/common/http';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { finalize } from 'rxjs/operators';
-
 import { IUgovor, Ugovor } from '../ugovor.model';
 import { UgovorService } from '../service/ugovor.service';
+
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import {IPonudjaci} from "app/entities/otvoreni/ponudjaci/ponudjaci.model";
+import {PonudjaciService} from "app/entities/otvoreni/ponudjaci/service/ponudjaci.service";
 
 @Component({
   selector: 'jhi-ugovor-update',
   templateUrl: './ugovor-update.component.html',
+  styleUrls: ['./ugovor.scss'],
 })
 export class UgovorUpdateComponent implements OnInit {
   isSaving = false;
-
-  editForm = this.fb.group({
-    id: [],
-    brojUgovora: [],
-    datumUgovora: [],
-    brojOdluke: [],
-    datumOdluke: [],
-    iznosUgovoraBezPdf: [],
-    sifraPostupka: [],
-    sifraPonude: [],
-  });
-
-  constructor(protected ugovorService: UgovorService, protected activatedRoute: ActivatedRoute, protected fb: FormBuilder) {}
-
-  ngOnInit(): void {
-    this.activatedRoute.data.subscribe(({ ugovor }) => {
-      this.updateForm(ugovor);
+  ponudjacis?: IPonudjaci[];
+  nadji?: any;
+  editForm: FormGroup;
+  aktivno: boolean;
+  constructor(
+    protected ugovorService: UgovorService,
+    protected activatedRoute: ActivatedRoute,
+    protected fb: FormBuilder,
+    protected ponudjaciService: PonudjaciService,
+    public dialogRef: MatDialogRef<UgovorUpdateComponent>,
+    @Inject(MAT_DIALOG_DATA)
+      { id, brojUgovora, brojOdluke, datumOdluke, datumUgovora, iznosUgovoraBezPdf, sifraPostupka, sifraPonude, sifraPonudjaca, name }: any
+  ) {
+    this.editForm = this.fb.group({
+      id: [id],
+      brojUgovora: [brojUgovora],
+      datumUgovora: [datumUgovora],
+      brojOdluke: [brojOdluke],
+      datumOdluke: [datumOdluke],
+      iznosUgovoraBezPdf: [iznosUgovoraBezPdf],
+      sifraPostupka: [sifraPostupka],
+      sifraPonude: [sifraPonude],
+      sifraPonudjaca: [sifraPonudjaca],
     });
+    this.aktivno = name;
   }
-
-  previousState(): void {
-    window.history.back();
+  public confirmAdd(): void {
+    const ugovor = this.createFromForm();
+    this.subscribeToSaveResponse(this.ugovorService.create(ugovor));
+    this.dialogRef.close();
   }
 
   save(): void {
     this.isSaving = true;
     const ugovor = this.createFromForm();
-    if (ugovor.id !== undefined) {
-      this.subscribeToSaveResponse(this.ugovorService.update(ugovor));
-    } else {
-      this.subscribeToSaveResponse(this.ugovorService.create(ugovor));
-    }
+    this.subscribeToSaveResponse(this.ugovorService.update(ugovor));
+  }
+  close(): any {
+    this.dialogRef.close();
   }
 
+  public getPonudjaci(): void {
+    this.ponudjaciService.query1().subscribe(res => {
+      this.ponudjacis = res;
+    });
+  }
+  ngOnInit(): void {
+    this.getPonudjaci();
+  }
   protected subscribeToSaveResponse(result: Observable<HttpResponse<IUgovor>>): void {
-    result.pipe(finalize(() => this.onSaveFinalize())).subscribe(
-      () => this.onSaveSuccess(),
-      () => this.onSaveError()
-    );
+    result.pipe(finalize(() => this.onSaveFinalize())).subscribe();
   }
-
-  protected onSaveSuccess(): void {
-    this.previousState();
-  }
-
-  protected onSaveError(): void {
-    // Api for inheritance.
-  }
-
   protected onSaveFinalize(): void {
     this.isSaving = false;
   }
-
-  protected updateForm(ugovor: IUgovor): void {
-    this.editForm.patchValue({
-      id: ugovor.id,
-      brojUgovora: ugovor.brojUgovora,
-      datumUgovora: ugovor.datumUgovora,
-      brojOdluke: ugovor.brojOdluke,
-      datumOdluke: ugovor.datumOdluke,
-      iznosUgovoraBezPdf: ugovor.iznosUgovoraBezPdf,
-      sifraPostupka: ugovor.sifraPostupka,
-      sifraPonude: ugovor.sifraPonude,
-    });
-  }
-
   protected createFromForm(): IUgovor {
     return {
       ...new Ugovor(),
@@ -91,6 +84,7 @@ export class UgovorUpdateComponent implements OnInit {
       iznosUgovoraBezPdf: this.editForm.get(['iznosUgovoraBezPdf'])!.value,
       sifraPostupka: this.editForm.get(['sifraPostupka'])!.value,
       sifraPonude: this.editForm.get(['sifraPonude'])!.value,
+
     };
   }
 }
